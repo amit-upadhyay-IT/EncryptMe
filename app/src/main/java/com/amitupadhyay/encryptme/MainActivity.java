866f3fd4cda719de033b68f61b199c40a5d1229b
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-05-12 05:04:32 +0530

    implementation completed

---
+++ app/src/main/java/com/amitupadhyay/encryptme/MainActivity.java
@@ -0,0 +1,61 @@
+package com.amitupadhyay.encryptme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.security.GeneralSecurityException;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView simpleMessage, encryptMessage, decryptMessage;
+
+    private String encryptedMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        simpleMessage = (TextView) findViewById(R.id.simpleMessage);
+        encryptMessage = (TextView) findViewById(R.id.encryptMessage);
+        decryptMessage = (TextView) findViewById(R.id.decryptMessage);
+
+
+        setSimpleMessage();
+        doEncryption();
+        doDecryptMessage();
+
+    }
+
+    private void setSimpleMessage()
+    {
+        simpleMessage.setText("AmitUpadhyay");
+    }
+
+    private void doEncryption()
+    {
+        String mykey = "password";
+        String message = "AmitUpadhyay";
+
+        try {
+            encryptedMsg = AESCrypt.encrypt(mykey, message);
+            encryptMessage.setText(encryptedMsg);
+        }catch (GeneralSecurityException e){
+            encryptMessage.setText("general security message");
+            //handle error
+        }
+    }
+
+    private void doDecryptMessage()
+    {
+        String mykey = "password";
+        try {
+            String messageAfterDecrypt = AESCrypt.decrypt(mykey, encryptedMsg);
+            decryptMessage.setText(messageAfterDecrypt);
+        }catch (GeneralSecurityException e){
+            //handle error - could be due to incorrect password or tampered encryptedMsg
+            decryptMessage.setText("general security exception");
+        }
+    }
+}
